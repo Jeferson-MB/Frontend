3921b8684ca4ad4c0ee0180656341ac7782f12c6
Jeferson-MB <jefersonmencosc7@gmail.com>
2025-06-04 14:49:45 -0600

    Add files via upload

--- javascript/auth/login.js
+++ javascript/auth/login.js
@@ -11,9 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       fetch('http://127.0.0.1:5000/api/login', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
       })
         .then(async res => {
@@ -35,4 +33,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-
